4a41720fb2709fffe3a608d8eaa196799ae05a72
Rachid Oussanaa <rchoood@gmail.com>
2014-02-01 01:37:04 +0000

    second

--- urls.py
+++ urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'hellodjango.views.home', name='home'),
     url(r'^analyze', 'hellodjango.views.analyze', name='analyze'),
-
     url(r'^admin/', include(admin.site.urls)),
 )
